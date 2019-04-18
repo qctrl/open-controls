66f259ea2a761efd6f1f6429113364e514ee78d0
virginia-m <virginia.m.frey@gmail.com>
2019-04-18 19:18:57 +1000

    add tests for get_plot_formatted_arrays and helper methods

--- tests/test_driven_controls.py
+++ tests/test_driven_controls.py
@@ -23,6 +23,7 @@ import pytest
 
 from qctrlopencontrols.exceptions import ArgumentsValueError
 from qctrlopencontrols import DrivenControls
+from qctrlopencontrols.globals import CARTESIAN, CYLINDRICAL
 
 
 def _remove_file(filename):
@@ -110,3 +111,70 @@ def test_control_export():
     _remove_file('driven_control_qctrl_cartesian.csv')
     _remove_file('driven_control_qctrl_cylindrical.json')
     _remove_file('driven_control_qctrl_cartesian.json')
+
+def test_plot_data():
+    """
+    Test the plot data produced for a driven control.
+    """
+
+    segments = [[1., 0., 0., 2.],
+                [0., 1.5, 1.7, 3.],
+                [1., 0., 2.1, 0.5]]
+    x_amplitude = [0., 1., 1., 0., 0., 1., 1., 0.]
+    y_amplitude = [0., 0., 0., 1.5, 1.5, 0., 0., 0.]
+    z_amplitude = [0., 0., 0., 1.7, 1.7, 2.1, 2.1, 0.]
+    times = [0., 0., 2., 2., 5., 5., 5.5, 5.5]
+    driven_control = DrivenControls(segments=segments)
+    plot_data = driven_control.get_plot_formatted_arrays(dimensionless=False)
+
+    assert np.allclose(plot_data['times'], times)
+    assert np.allclose(plot_data['x_amplitudes'], x_amplitude)
+    assert np.allclose(plot_data['y_amplitudes'], y_amplitude)
+    assert np.allclose(plot_data['z_amplitudes'], z_amplitude)
+
+def test_dimensionless_segments():
+    """
+    Test the dimensionless amplitude and angle segments generated
+    """
+    segments = [[1., 0., 0., np.pi / 2],
+                [0., 1., 0., np.pi / 2],
+                [1. / np.sqrt(2.), 0., 1. / np.sqrt(2.), np.pi / 2]]
+
+    _on_resonance_amplitudes = np.array([1., 1., 1. / np.sqrt(2.)])
+    _azimuthal_angles = np.array([0., np.pi / 2, 0.])
+    _detunings = np.array([0, 0, 1. / np.sqrt(2.)])
+    _durations = np.pi / 2. * np.array([1., 1., 1.])
+
+    amplitude_angle_segments = np.stack((_on_resonance_amplitudes, _azimuthal_angles,
+                                         _detunings, _durations), axis=1)
+
+    driven_control = DrivenControls(segments=segments)
+    _max_rabi = driven_control.maximum_rabi_rate
+
+    dimensionless_euclid = segments.copy()
+    dimensionless_euclid = np.array(dimensionless_euclid)
+    dimensionless_euclid[:, 0:2] = dimensionless_euclid[:, 0:2] / _max_rabi
+
+    dimensionless_cylinder = amplitude_angle_segments.copy()
+    dimensionless_cylinder = np.array(dimensionless_cylinder)
+    dimensionless_cylinder[:, 0] = dimensionless_cylinder[:, 0] / _max_rabi
+
+    transformed_euclidean = driven_control.get_transformed_segments(coordinates=CARTESIAN,
+                                                                    dimensionless=False)
+
+    assert np.allclose(segments, transformed_euclidean)
+
+    transformed_euclidean = driven_control.get_transformed_segments(coordinates=CARTESIAN,
+                                                                    dimensionless=True)
+
+    assert np.allclose(dimensionless_euclid, transformed_euclidean)
+
+    transformed_cylindrical = driven_control.get_transformed_segments(coordinates=CYLINDRICAL,
+                                                                      dimensionless=False)
+
+    assert np.allclose(amplitude_angle_segments, transformed_cylindrical)
+
+    transformed_cylindrical = driven_control.get_transformed_segments(coordinates=CYLINDRICAL,
+                                                                      dimensionless=True)
+
+    assert np.allclose(amplitude_angle_segments, transformed_cylindrical)
