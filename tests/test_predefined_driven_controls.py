3e9d2a04a6184108cbb1b10d852659499ba4a8d5
virginia-m <virginia.m.frey@gmail.com>
2019-04-20 08:11:32 +1000

    add tests for SK1 and SCROFULOUS

--- tests/test_predefined_driven_controls.py
+++ tests/test_predefined_driven_controls.py
@@ -24,11 +24,12 @@ from qctrlopencontrols.exceptions import ArgumentsValueError
 
 from qctrlopencontrols.driven_controls import (
     new_predefined_driven_control,
-    new_primitive_control, new_wimperis_1_control#, new_solovay_kitaev_1_control,
+    new_primitive_control, new_wimperis_1_control, new_solovay_kitaev_1_control,
+    PRIMITIVE, WIMPERIS_1, SOLOVAY_KITAEV_1,
+    new_short_composite_rotation_for_undoing_length_over_and_under_shoot_control
     #new_compensating_for_off_resonance_with_a_pulse_sequence_control,
     #new_compensating_for_off_resonance_with_a_pulse_sequence_with_solovay_kitaev_control,
     #new_compensating_for_off_resonance_with_a_pulse_sequence_with_wimperis_control,
-    #new_short_composite_rotation_for_undoing_length_over_and_under_shoot_control,
     #new_walsh_amplitude_modulated_filter_1_control,
     #new_corpse_in_scrofulous_control
 )
@@ -43,6 +44,22 @@ def test_new_predefined_driven_control():
     # Test that an error is raised if supplied with an unknown scheme
     with pytest.raises(ArgumentsValueError):
         _ = new_predefined_driven_control(scheme='nil')
+
+def test_predefined_common_attributes():
+    """Test that expected exceptions are raised correctly for invalid parameters
+    """
+    # Test negative maximum Rabi rate
+    with pytest.raises(ArgumentsValueError):
+        _ = new_predefined_driven_control(
+            maximum_rabi_rate=-1, shape='PRIMITIVE', rabi_rotation=1, azimuthal_angle=0)
+    # Test invalid shape
+    with pytest.raises(ArgumentsValueError):
+        _ = new_predefined_driven_control(
+            maximum_rabi_rate=1, shape='-', rabi_rotation=1, azimuthal_angle=0)
+    # Test zero Rabi rotation
+    with pytest.raises(ArgumentsValueError):
+        _ = new_predefined_driven_control(
+            maximum_rabi_rate=1, shape='PRIMITIVE', rabi_rotation=0, azimuthal_angle=0)
 
 
 def test_primitive_control_segments():
@@ -70,13 +87,14 @@ def test_primitive_control_segments():
         maximum_rabi_rate=_rabi_rate,
         azimuthal_angle=_azimuthal_angle,
         shape=SQUARE,
-        scheme='primitive'
+        scheme=PRIMITIVE
     )
 
     assert np.allclose(_segments, primitive_control_1.segments)
     assert np.allclose(_segments, primitive_control_2.segments)
 
-def test_new_wimperis_1_control():
+
+def test_wimperis_1_control():
     """Test the segments of the Wimperis 1 (BB1) driven control
     """
     _rabi_rotation = np.pi
@@ -92,10 +110,94 @@ def test_new_wimperis_1_control():
         [np.cos(phi_p + _azimuthal_angle), np.sin(phi_p + _azimuthal_angle), 0., np.pi]
     ]
 
-    wimperis_control = new_wimperis_1_control(
+    wimperis_control_1 = new_wimperis_1_control(
+        rabi_rotation=_rabi_rotation,
+        azimuthal_angle=_azimuthal_angle,
+        maximum_rabi_rate=1
+    )
+    wimperis_control_2 = new_predefined_driven_control(
+        rabi_rotation=_rabi_rotation,
+        azimuthal_angle=_azimuthal_angle,
+        maximum_rabi_rate=1,
+        scheme=WIMPERIS_1
+    )
+
+    assert np.allclose(wimperis_control_1.segments, _segments)
+    assert np.allclose(wimperis_control_2.segments, _segments)
+
+
+def test_solovay_kitaev_1_control():
+    """Test the segments of the Solovay-Kitaev 1 (SK1) driven control
+    """
+    _rabi_rotation = np.pi
+    _azimuthal_angle = np.pi/2
+
+    phi_p = np.arccos(-_rabi_rotation / (4 * np.pi))
+
+    _segments = [
+        [np.cos(_azimuthal_angle), np.sin(_azimuthal_angle), 0., _rabi_rotation],
+        [np.cos(-phi_p + _azimuthal_angle), np.sin(-phi_p + _azimuthal_angle), 0., 2 * np.pi],
+        [np.cos(phi_p + _azimuthal_angle), np.sin(phi_p + _azimuthal_angle), 0., 2 * np.pi]
+    ]
+
+    sk1_control_1 = new_solovay_kitaev_1_control(
         rabi_rotation=_rabi_rotation,
         azimuthal_angle=_azimuthal_angle,
         maximum_rabi_rate=1
     )
 
-    assert np.allclose(wimperis_control.segments, _segments)
+    sk1_control_2 = new_predefined_driven_control(
+        scheme=SOLOVAY_KITAEV_1,
+        rabi_rotation=_rabi_rotation,
+        azimuthal_angle=_azimuthal_angle,
+        maximum_rabi_rate=1
+    )
+
+    assert np.allclose(sk1_control_1.segments, _segments)
+    assert np.allclose(sk1_control_2.segments, _segments)
+
+def test_scofulous_control():
+    """Test the segments of the SCROFULOUS driven control.
+       Note: here we test against numerical pulse segments since the angles are
+       defined numerically as well.
+    """
+    # Test that exceptions are raised upon wrong inputs for rabi_rotation
+    # (SCROFULOUS is only defined for pi/4, pi/2 and pi pulses)
+    with pytest.raises(ArgumentsValueError):
+        _ = new_short_composite_rotation_for_undoing_length_over_and_under_shoot_control(
+            rabi_rotation=0.3
+        )
+
+    # Construct SCROFULOUS controls for target rotations pi/4, pi/2 and pi
+    pi_segments = new_short_composite_rotation_for_undoing_length_over_and_under_shoot_control(
+        rabi_rotation=np.pi, azimuthal_angle=0.5, maximum_rabi_rate=2*np.pi
+    ).segments
+
+    _pi_segments = np.array([
+        [0.14826172, 6.28143583, 0., 0.5],
+        [5.36575214, -3.26911633, 0., 0.5],
+        [0.14826172, 6.28143583, 0., 0.5]])
+
+    assert np.allclose(pi_segments, _pi_segments)
+
+    pi_on_2_segments = new_short_composite_rotation_for_undoing_length_over_and_under_shoot_control(
+        rabi_rotation=np.pi/2, azimuthal_angle=-0.5, maximum_rabi_rate=2*np.pi
+    ).segments
+
+    _pi_on_2_segments = np.array([
+        [5.25211762, 3.44872124, 0., 0.32],
+        [-1.95046211, -5.97278119, 0., 0.5],
+        [5.25211762, 3.44872124, 0., 0.32]])
+
+    assert np.allclose(pi_on_2_segments, _pi_on_2_segments)
+
+    pi_on_4_segments = new_short_composite_rotation_for_undoing_length_over_and_under_shoot_control(
+        rabi_rotation=np.pi/4, azimuthal_angle=0, maximum_rabi_rate=2*np.pi
+    ).segments
+
+    _pi_on_4_segments = np.array([
+        [1.78286387, 6.0249327, 0., 0.26861111],
+        [0.54427724, -6.25956707, 0., 0.5],
+        [1.78286387, 6.0249327, 0., 0.26861111]])
+
+    assert np.allclose(pi_on_4_segments, _pi_on_4_segments)
