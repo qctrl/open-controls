2f8fa12ec8b7190c4c0821424cb734b37dd44535
CircleCI workflow <accounts@q-ctrl.com>
2019-06-18 07:24:11 +0000

    Update setup.py to match pyproject.toml [ci skip]

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import os.path
 
 readme = ''
 here = os.path.abspath(os.path.dirname(__file__))
-readme_path = os.path.join(here, 'README.rst')
+readme_path = os.path.join(here, 'README.md')
 if os.path.exists(readme_path):
     with open(readme_path, 'rb') as stream:
         readme = stream.read().decode('utf8')
