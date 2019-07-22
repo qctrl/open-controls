5d6bbccff1516dee3e2911198406fc4ba760dc9c
CircleCI workflow <accounts@q-ctrl.com>
2019-07-22 04:00:29 +0000

    Bump version to 2.0.0 [ci skip]

--- qctrlopencontrols/__init__.py
+++ qctrlopencontrols/__init__.py
@@ -18,7 +18,7 @@ qctrlopencontrols
 =================
 """
 
-__version__ = "1.2.1"
+__version__ = "2.0.0"
 
 from .cirq.circuit import convert_dds_to_cirq_circuit
 from .cirq.schedule import convert_dds_to_cirq_schedule
