beb0d252491dd46edc2f5c93f8983942bf2353b3
Steve Gore <steve.j.gore@gmail.com>
2019-09-20 16:52:56 +1000

    Change import path

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 import toml
 
-sys.path.insert(0, os.path.abspath('../qctrlopencontrols'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 parsed = toml.load("../pyproject.toml")
