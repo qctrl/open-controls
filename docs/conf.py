fd9d5ce7db125c87d4a81b73f8e45f6b0b8a02e9
Steve Gore <steve.j.gore@gmail.com>
2020-01-20 16:02:12 +1100

    Remove duplication

--- docs/conf.py
+++ docs/conf.py
@@ -61,69 +61,7 @@ html_static_path = ['_static']
 html_css_files = [
     'css/custom.css',
 ]
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
-import sys
-import toml
-
-sys.path.insert(0, os.path.abspath(".."))
-
-# -- Project information -----------------------------------------------------
-parsed = toml.load("../pyproject.toml")
-package_info = parsed["tool"]["poetry"]
-project = package_info["description"]
-author = ", ".join(package_info["authors"])
-release = package_info["version"]
-copyright = "2019, Q-CTRL <support@q-ctrl.com>"
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named "sphinx.ext.*") or your custom
-# ones.
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.coverage",
-    "sphinx.ext.mathjax",
-    "sphinx.ext.napoleon"
-]
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
-master_doc = "index"
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "default"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
-html_css_files = [
-    "css/custom.css",
-]
 html_js_files = [
     "https://dev.docs.q-ctrl.com/assets/js/sphinx.js",
 ]
-html_logo = "http://dev.docs.q-ctrl.com/assets/img/header_logo_sphinx.svg"
+html_logo = "https://dev.docs.q-ctrl.com/assets/img/header_logo_sphinx.svg"
