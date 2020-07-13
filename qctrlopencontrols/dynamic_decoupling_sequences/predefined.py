Fix post rotation

qctrlopencontrols/dynamic_decoupling_sequences/predefined.py
@@ -42,9 +42,9 @@ def _add_pre_post_rotations(
     """Adds a pre and post X rotation at the start and end of the sequence.
 
     Note that with these two pre and post X rotations, the net effect of the DDS does not
-    necessarily have to be an identity. For example, given a CPMG sequence of odd number Y pi
-    rotations in the middle with the pre (pi/2) and post(-pi/2) X rotations, the net effect will
-    be a Z gate.
+    necessarily have to be an identity, but it will always be either an identity or Z pi rotation.
+    For example, given a CPMG sequence of odd number Y pi rotations in the middle with the pre
+    (pi/2) and post(-pi/2) X rotations, the net effect will be a Z gate.
 
     This function assumes that the sequences only have X, Y, and Z pi-pulses.
     An exception is thrown if that is not the case.
@@ -130,7 +130,7 @@ def _add_pre_post_rotations(
     # use pi/2 and -pi/2 (pi/2) X pulses as pre and post rotations for CP sequences with
     # odd(even) number of pulses
     # always use pi/2 and -pi/2 X pulses as pre and post rotations for CPMG sequences
-    if preserves_10 or (not preserves_11):
+    if (preserves_10 and preserves_11) or (not preserves_10 and not preserves_11):
         final_azimuthal = np.pi
 
     offsets = np.insert(offsets, [0, offsets.shape[0]], [0, duration],)

tests/test_predefined_dynamical_decoupling.py
@@ -680,7 +680,7 @@ def _pulses_produce_identity(sequence, extra_rotation=None):
     We check this by creating the unitary of each pulse and then multiplying them
     by each other to check the complete evolution.
 
-    However, note that DDS sequence does not necessarily have to produce an identity gate.
+    Note that the net effect of DDS sequence is either an identity gate or a Z pi rotation.
     For example, CPMG sequence with odd number of Y pulses produces a Z rotation.
     ``extra_rotation`` is used to compensate this net effect.
     """
@@ -948,7 +948,7 @@ def test_if_quadratic_sequence_with_odd_inner_pulses_is_identity():
     assert len(inner_odd_quadratic_sequence.offsets) == 8 + 7 * (8 + 1) + 2
 
     assert _pulses_produce_identity(
-        inner_odd_quadratic_sequence, extra_rotation=np.matmul(SIGMA_X, -SIGMA_Z)
+        inner_odd_quadratic_sequence, extra_rotation=SIGMA_Z
     )
 
 