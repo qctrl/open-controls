47ff705cac01f6b5f402d2f5ecc93319d60e9651
Christopher Farrenden <christopher.farrenden@q-ctrl.com>
2021-01-12 14:07:29 +1100

    Apply pyproject.toml standard (#167)
    * Apply pyproject.toml standard
    
    * Update setup.py/README.rst to match pyproject.toml/README.md
    
    * Open controls documentation link
    
    * Update setup.py/README.rst to match pyproject.toml/README.md
    
    * Add repository
    
    * Update setup.py/README.rst to match pyproject.toml/README.md
    
    Co-authored-by: Q-CTRL DevOps <devops@q-ctrl.com>

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import os.path
 
 readme = ''
 here = os.path.abspath(os.path.dirname(__file__))
-readme_path = os.path.join(here, 'README.rst')
+readme_path = os.path.join(here, 'DESCRIPTION.rst')
 if os.path.exists(readme_path):
     with open(readme_path, 'rb') as stream:
         readme = stream.read().decode('utf8')
@@ -27,12 +27,12 @@ setup(
     version='7.0.0',
     description='Q-CTRL Open Controls',
     python_requires='<3.9,>=3.6.4',
-    project_urls={"repository": "https://github.com/qctrl/python-open-controls"},
+    project_urls={"documentation": "https://docs.q-ctrl.com/references/python/qctrl-open-controls/", "homepage": "https://q-ctrl.com", "repository": "https://github.com/qctrl/python-open-controls"},
     author='Q-CTRL',
     author_email='support@q-ctrl.com',
     license='Apache-2.0',
-    keywords='quantum computing open source engineering',
-    classifiers=['Development Status :: 5 - Production/Stable', 'Environment :: Console', 'Intended Audience :: Developers', 'Intended Audience :: Education', 'Intended Audience :: Science/Research', 'License :: OSI Approved :: Apache Software License', 'Natural Language :: English', 'Operating System :: OS Independent', 'Programming Language :: Python :: 3.6', 'Topic :: Scientific/Engineering :: Physics', 'Topic :: Scientific/Engineering :: Visualization', 'Topic :: Software Development :: Embedded Systems', 'Topic :: System :: Distributed Computing'],
+    keywords='q-ctrl qctrl quantum control',
+    classifiers=['Development Status :: 5 - Production/Stable', 'Environment :: Console', 'Intended Audience :: Developers', 'Intended Audience :: Education', 'Intended Audience :: Science/Research', 'Natural Language :: English', 'Operating System :: OS Independent', 'Programming Language :: Python :: 3.6', 'Programming Language :: Python :: 3.7', 'Programming Language :: Python :: 3.8', 'Topic :: Internet :: WWW/HTTP', 'Topic :: Scientific/Engineering :: Physics', 'Topic :: Scientific/Engineering :: Visualization', 'Topic :: Software Development :: Embedded Systems', 'Topic :: System :: Distributed Computing'],
     packages=['qctrlopencontrols', 'qctrlopencontrols.driven_controls', 'qctrlopencontrols.dynamic_decoupling_sequences'],
     package_dir={"": "."},
     package_data={},
