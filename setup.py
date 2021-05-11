13524e84b71d7123f9b2215e69ee6ee420861086
Leonardo Andreta de Castro <leonardo.andreta@q-ctrl.com>
2021-05-11 16:17:55 +1000

    feat: Make Open Controls compatible with Python 3.9 (#187)
    I tried locally in my computer, and this seems to run fine in Python 3.9, so I don't think we need to restrict our users to older versions of Python.
    
    Changes proposed in this pull request:
    
    - Lift restriction that Open Controls is only compatible with Python < 3.9
    - Add unit tests for Python 3.9 in the CI.
    - Update name of the docker images in the CI.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     name='qctrl-open-controls',
     version='8.1.0',
     description='Q-CTRL Python Open Controls',
-    python_requires='<3.9,>=3.6.4',
+    python_requires='<3.10,>=3.6.4',
     project_urls={"documentation": "https://docs.q-ctrl.com/references/python/qctrl-open-controls/", "homepage": "https://q-ctrl.com", "repository": "https://github.com/qctrl/python-open-controls"},
     author='Q-CTRL',
     author_email='support@q-ctrl.com',
