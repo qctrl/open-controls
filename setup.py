7e49a7d47b5d9fa5a48c56d56d1e327d0d8e5aa3
Q-CTRL DevOps <devops@q-ctrl.com>
2021-10-27 00:15:59 +0000

    Update setup.py/README.rst to match pyproject.toml/README.md

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if os.path.exists(readme_path):
 setup(
     long_description=readme,
     name='qctrl-open-controls',
-    version='8.3.0',
+    version='8.4.0',
     description='Q-CTRL Python Open Controls',
     python_requires='<3.10,>=3.6.4',
     project_urls={"documentation": "https://docs.q-ctrl.com/open-controls/references/qctrl-open-controls/", "homepage": "https://q-ctrl.com", "repository": "https://github.com/qctrl/python-open-controls"},
