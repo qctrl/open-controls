55585b6a3290a889c30984ddfd7d604a6fa765a7
Q-CTRL DevOps <devops@q-ctrl.com>
2022-02-11 02:53:57 +0000

    Bump version to 9.0.0

--- qctrlopencontrols/__init__.py
+++ qctrlopencontrols/__init__.py
@@ -16,7 +16,7 @@
 Top-level package for Q-CTRL Open Controls.
 """
 
-__version__ = "8.4.0"
+__version__ = "9.0.0"
 
 from .driven_controls.driven_control import DrivenControl
 from .driven_controls.predefined import (
