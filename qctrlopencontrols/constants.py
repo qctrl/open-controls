84cefca171b20a163657ba7503c2cd0d9fe75c4d
Q-CTRL Robot <robot@q-ctrl.com>
2024-01-30 06:37:10 +0000

    chore: Update licensing comments

--- qctrlopencontrols/constants.py
+++ qctrlopencontrols/constants.py
@@ -1,4 +1,4 @@
-# Copyright 2023 Q-CTRL
+# Copyright 2024 Q-CTRL
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
